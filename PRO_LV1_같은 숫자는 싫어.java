1a4371abc3568bf62a3078b758b7b6bf1011da8f
HoJun <76953561+HoJun-Song@users.noreply.github.com>
2022-03-11 17:23:33 +0900

    Create PRO_LV1_같은 숫자는 싫어.java

---
+++ PRO_LV1_같은 숫자는 싫어.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> myclass = new ArrayList<>();
+        myclass.add(arr[0]);
+        int cnt = 0;
+        
+        for (int i = 1; i < arr.length; i++) {
+            int tmp = myclass.get(cnt);
+            
+            if (tmp != arr[i]) {
+                myclass.add(arr[i]);
+                cnt++;
+            }
+            else {
+                continue;
+            }
+            
+        }
+        
+        int[] answer = myclass.stream().mapToInt(Integer::intValue).toArray();
+        
+        return answer;
+    }
+}
