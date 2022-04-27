199a1600a0079c16cd5704b6a1dccb10cb309e97
HoJun <76953561+HoJun-Song@users.noreply.github.com>
2022-04-27 20:50:17 +0900

    Create PRO_LV1_신규아이디추천.java

---
+++ PRO_LV1_신규아이디추천.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^.\\-_\\w]*","");
+        new_id = new_id.replaceAll("\\.{2,}",".");
+        new_id = new_id.replaceAll("^\\.","");
+        new_id = new_id.replaceAll("\\.$","");
+        if (new_id.equals("")) new_id += "a";
+        if (new_id.length() >= 16) new_id = new_id.substring(0, 15);
+        new_id = new_id.replaceAll("\\.$","");
+
+        while (new_id.length() < 3) {
+            new_id += new_id.substring(new_id.length() - 1);
+        }
+
+        answer = new_id;
+        return answer;
+    }
+}
