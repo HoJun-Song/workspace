796dc112288119d7a1257d951a982afc1c695ee7
HoJun <sson514@naver.com>
2022-05-21 15:31:29 +0900

    2022-05-21

---
+++ PRO_LV2_문자열압축.py
@@ -0,0 +1,39 @@
+#2022-05-21 1차시도
+def solution(s):
+    answer = 0
+    unit = 1
+    answers = []
+    
+    while(unit <= len(s)):
+        tmp = s[0:unit]
+        result = ""
+        count = 1
+        i = unit
+
+        while i < (len(s) - 1):
+            cmp = s[i:(i+unit)]
+            if cmp == tmp:
+                count += 1
+            else:
+                if count != 1:
+                    result += str(count) + tmp
+                else:
+                    result += tmp
+                count = 1
+                tmp = cmp
+                
+            i += unit
+
+        if count != 1:
+            result += str(count) + tmp
+        else:
+            result += tmp
+            result += s[i:]
+        
+        answers.append(len(result))
+            
+        unit += 1
+        
+    answer = min(answers)
+    
+    return answer
