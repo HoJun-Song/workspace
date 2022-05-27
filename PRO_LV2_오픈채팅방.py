fa3a2086db4aee43931052c8e29171f812be21b8
Hojun Song <sson514@naver.com>
2022-05-27 16:35:10 +0900

    2022-05-27 clear

--- PRO_LV2_오픈채팅방.py
+++ PRO_LV2_오픈채팅방.py
@@ -1,23 +1,19 @@
-#2022-05-21 2차시도
 def solution(record):
     answer = []
-    tmp = []
+    dictonary = {}
+    
     for sentence in record:
         act = sentence.split(" ")
-        if act[0] == 'Enter':
-            tmp.append([act[1], act[2], "님이 들어왔습니다."])
-            for i in tmp:
-                if i[0] == act[1]:
-                    i[1] = act[2]
-        elif act[0] == 'Leave':
-            for i in tmp:
-                if i[0] == act[1]:
-                    tmp.append([act[1], i[1], "님이 나갔습니다."])
-                    break
-        else:
-            for i in tmp:
-                if i[0] == act[1]:
-                    i[1] = act[2]
-    for i in tmp:
-        answer.append(i[1] + i[2])
+        if len(act) > 2:
+            dictonary[act[1]] = act[2]
+            
+    for sentence in record:
+        act = sentence.split(" ")
+        if act[0] == "Enter":
+            user = dictonary.get(act[1])
+            answer.append(user + "님이 들어왔습니다.")
+        elif act[0] == "Leave":
+            user = dictonary.get(act[1])
+            answer.append(user + "님이 나갔습니다.")
+    
     return answer
