495e41efe036e1868e7baff2ee4f1e159ec7a881
Hojun Song <sson514@naver.com>
2022-05-28 21:03:11 +0900

    2022-05-28 1차 시도

---
+++ PRO_LV2_124나라의숫자.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = ''
+    arr = []
+    n = int(n)
+    
+    while n >= 1:
+        tmp = int(n % 3)
+        arr.append(tmp)
+        n = int(n / 3)
+        
+    arr.reverse()
+    
+    for i in arr:
+        answer += str(i)
+    
+    return answer
