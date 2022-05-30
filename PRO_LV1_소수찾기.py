4d2258169f30d59a76130b9181cf4b0f7b9e868f
Hojun Song <sson514@naver.com>
2022-05-30 21:44:25 +0900

    2022-05-30 1차 시도

---
+++ PRO_LV1_소수찾기.py
@@ -0,0 +1,23 @@
+#2022-05-30 1차 시도
+def solution(n):
+    answer = 0
+    flag = 1
+    
+    for i in range(n):
+        
+        for j in range(i):
+            target = i + 1
+            tmp = j + 1
+            
+            if tmp != 1 and tmp != target:
+                if target % tmp == 0:
+                    flag = 1
+                    break
+                
+        if flag == 1:
+            flag = 0
+            continue
+        
+        answer += 1
+        
+    return answer
