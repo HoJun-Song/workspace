8701ccf378b1cd53953740d248396af885d4b07b
Hojun Song <sson514@naver.com>
2022-07-02 20:45:33 +0900

    2022-07-02

---
+++ PRO_LV1_핸드폰번호가리기.py
@@ -0,0 +1,8 @@
+def solution(phone_number):
+    
+    phoneLength = len(phone_number)
+    answer = list(phone_number)
+    for i in range(phoneLength - 4):
+        answer[i] = '*'
+    answer = "".join(answer)
+    return answer
