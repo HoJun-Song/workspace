d5e4d6c039e908b8cbb3b2683c0b47dfe234e87f
Hojun Song <sson514@naver.com>
2022-07-08 18:36:18 +0900

    2022-07-08

--- PRO_LV2_위장.py
+++ PRO_LV2_위장.py
@@ -1,29 +1,20 @@
-#2022-07-06 1차 시도
 def solution(clothes):
     clothesType = []
-    clothesType.append([clothes[0][1], 1])
-    
-    for i in range(1, len(clothes)):
-        tmp = clothes[i][1]
+    for i in clothes:
+        tmp = i[1]
         flag = 0
-        
         for j in range(len(clothesType)):
-            if tmp in clothesType[j][0]:
+            if tmp == clothesType[j][0]:
                 flag = 1
                 clothesType[j][1] += 1
-                
+                break
+            
         if flag == 0:
-            clothesType.append([tmp, 1])
+            clothesType.append([i[1], 1])
     
-    print(clothesType)
-    
-    if len(clothesType) > 1:
-        tmp = 1
-        for i in clothesType:
-            tmp *= (i[1] + 1)   
-        answer = tmp - 1
-        
-    else:
-        answer = clothesType[0][1]
+    answer = 1
+    for i in clothesType:
+        answer *= (i[1] + 1)
+    answer -= 1
         
     return answer
